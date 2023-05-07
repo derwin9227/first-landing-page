6d60369cf78585e5a3144fecccc80dd774bf22c9
derwin <derwin9227@gmail.com>
2023-05-07 00:59:06 -0500

    agregando estructura sass/react y terminando navbar

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='home' id='home'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ipsa facilis, aliquid nemo tempore architecto nostrum reprehenderit odit qui quis corporis consectetur nisi dolorum iure mollitia dolor, illum, unde a.</p>
+    </section>
+  )
+}
+
+export default Home
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div id="navbar" className='navbar'>
+        <nav className='navbar__list'>
+            <li className='navbar__list-item'><a className='navbar__list-item_link' href="#navbar">Home</a></li>
+            <li className='navbar__list-item'><a className='navbar__list-item_link' href="#skills">Skills</a></li>
+            <li className='navbar__list-item'><a className='navbar__list-item_link' href="#about">About</a></li>
+            <li className='navbar__list-item'><a className='navbar__list-item_link' href="#footer">Footer</a></li>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
